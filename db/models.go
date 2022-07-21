8c7a8c835eb23f3f6c5f6a9df984ab640d24c3c4
PigeonsHouse <bullbull1230@gmail.com>
2022-07-21 17:05:27 +0900

    [delete] UpdatedAtの削除

--- db/models.go
+++ db/models.go
@@ -15,7 +15,6 @@ func (base *Base) BeforeCreate(tx *gorm.DB) (err error) {
 type Base struct {
 	ID        string    `gorm:"primaryKey" json:"id" sql:"type:uuid;primary_key;default:uuid_generate_v4()"`
 	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
 }
 
 type User struct {
